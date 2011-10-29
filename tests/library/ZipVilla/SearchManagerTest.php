c8bdb1f1c238be7d622ba4ee57740921f5e89610
Venki <venki_t@yahoo.com>
2011-10-29 14:02:40 +0530

    added slor facet code

---
+++ tests/library/ZipVilla/SearchManagerTest.php
@@ -0,0 +1,126 @@
+<?php
+include_once("ZipVilla/Helper/ListingsManager.php");
+include_once("ZipVilla/Helper/IndexManager.php");
+include_once("ZipVilla/Helper/SearchManager.php");
+include_once("ZipVilla/Utils.php");
+
+class IndexManagerTest extends PHPUnit_Framework_TestCase
+{
+
+	public function setUp()
+	{
+		$this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+		parent::setUp();
+		$seed_script = APPLICATION_PATH . "/../tests/library/ZipVilla/test_schema.js";
+		shell_exec("/usr/bin/mongo " . $seed_script);
+		//clear all the indexed document from solr
+		$im = new IndexManager();
+		$res = $im->delete();
+	}
+
+	function _insertListing1() {
+		$lm = new ZipVilla_Helper_ListingsManager();
+		$res = $lm->query();
+		$tname = "hotel";
+		$vals = array();
+		$vals['size'] = "15 acres"; 
+		$vals['activities'] = array('bonfire', 'nature walk','trekking');
+		$vals['specalities'] = array('house wines');
+		$vals['userId'] = "owner_5678";
+		$vals['streetNumber'] = "456-D";
+		$vals['street'] = "Calungute Road";
+		$vals['line1'] = "North Gao";
+		$vals['line2'] = "Beach resort";
+		$vals['city'] = "Mapusa";
+		$vals['state'] = "Goa";
+		$vals['country'] = "India";
+		$vals['pincode'] = "5676799";
+		$vals['neighbourhood'] = "near church";
+		$vals['lat'] = 55.6;
+		$vals['long'] = 65.8;
+		$vals['amenities'] = array('massage','sauna');
+		$vals['title'] = "The Beach Home"; 
+		$description = "very nice  place near fort";
+		$vals['description'] = $description;
+		$vals['images'] = array('content/one1.jpg','content/two1.jpg');
+		$vals['thumbnail'] = 'content/thumb1.jpg';
+		$vals['url'] = 'http://mycompany.com';
+		$vals['rate'] = 4600;
+		$vals['rateType'] = "double occupancy";
+		$vals['maxGuests'] = 6;
+
+		$lm = new ZipVilla_Helper_ListingsManager();
+		$res = $lm->insert($tname,$vals);
+
+		$this->assertEquals("hotel", $res->type, "Wrong 'Type' for inserted object."); 
+		$this->assertEquals("Goa", $res->address['state'], "Wrong 'State' for inserted object."); 
+		$this->assertEquals($description, $res->description, "Wrong 'Description' for inserted object.");
+		$this->assertTrue($res->id != null);
+		return $res->id;
+		
+	}
+	function _insertListing2() {
+		$lm = new ZipVilla_Helper_ListingsManager();
+		$res = $lm->query();
+		$tname = "resort";
+		$vals = array();
+		$vals['activities'] = array('bonfire', 'swimming');
+		$vals['specalities'] = array('goan food');
+		$vals['userId'] = "owner_5668";
+		$vals['streetNumber'] = "456-D";
+		$vals['street'] = "Calungute Road";
+		$vals['line1'] = "North Gao";
+		$vals['line2'] = "Beach resort";
+		$vals['city'] = "Calungute";
+		$vals['state'] = "Goa";
+		$vals['country'] = "India";
+		$vals['pincode'] = "5676719";
+		$vals['neighbourhood'] = "near church";
+		$vals['lat'] = 45.6;
+		$vals['long'] = 75.8;
+		$vals['amenities'] = array('health club','sauna');
+		$vals['title'] = "The Beach Home"; 
+		$description = "right by the beach";
+		$vals['description'] = $description;
+		$vals['images'] = array('content/one11.jpg','content/two11.jpg');
+		$vals['thumbnail'] = 'content/thumb11.jpg';
+		$vals['url'] = 'http://mycompany1.com';
+		$vals['rate'] = 8600;
+		$vals['rateType'] = "suite offer";
+		$vals['maxGuests'] = 6;
+
+		$lm = new ZipVilla_Helper_ListingsManager();
+		$res = $lm->insert($tname,$vals);
+
+		$this->assertEquals("resort", $res->type, "Wrong 'Type' for inserted object."); 
+		$this->assertEquals("Goa", $res->address['state'], "Wrong 'State' for inserted object."); 
+		$this->assertEquals($description, $res->description, "Wrong 'Description' for inserted object.");
+		$this->assertTrue($res->id != null);
+		return $res->id;
+		
+	}
+	public function _createData() {
+		$im = new IndexManager();
+		$id = $this->_insertListing1();
+		$res = $im->indexById($id);
+		$this->assertTrue($res->success());
+		$id = $this->_insertListing2();
+		$res = $im->indexById($id);
+		$this->assertTrue($res->success());			
+	}
+	public function testSearchFacets() {
+		$this->_createData();
+		$sm = new SearchManager();
+		$q = array("address_state"=>'Goa');
+		$fds = array('address_city','id','title');
+		$ffds = array('address_city','amenities');
+		$docs = $sm->search($q,$fds,$ffds);
+		$this->assertTrue($docs != null);
+		//print_r($docs);
+		$this->assertTrue(count($docs) == 1);
+		$d = $docs[0];
+		$t = $d['title'];
+		$this->assertEquals(2,count($t),"Wrong number of results");
+	}
+}		
+?>
