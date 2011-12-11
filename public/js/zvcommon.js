c1be623828e35577173d1c78b95cb273fbd1177c
Vijaya Raghavan <vijay@ltp.soft.net>
2011-12-11 13:17:21 +0530

    merged with sylvester's work.

--- public/js/zvcommon.js
+++ public/js/zvcommon.js
@@ -1,7 +1,7 @@
-$(document).ready(function()
-{
+$(document).ready(function() 
+{	
 	//select all the a tag with name equal to modal
-	$('a[name=lightbox_zv]').click(function(e)
+	$('a[name=lightbox_zv]').click(function(e) 
 	{
 		//Cancel the link behavior
 		e.preventDefault();
@@ -34,11 +34,12 @@ $(document).ready(function()
 	
 		//transition effect
 		$(id).fadeIn(2000); 
+		$(window).scrollTop($("#lb_search_zv").offset().top);
 	
 	});
 	
 	//if close button is clicked
-	$('.lb_window_zv .close').click(function (e)
+	$('.lb_window_zv .close').click(function (e) 
 	{
 		//Cancel the link behavior
 		e.preventDefault();
@@ -48,12 +49,51 @@ $(document).ready(function()
 	});		
 	
 	//if mask_zv is clicked
-	$('#mask_zv').click(function ()
+	$('#mask_zv').click(function () 
 	{
 		$(this).hide();
 		$('.lb_window_zv').hide();
 	});
 	
-	$('#tabs').tabs();			
+	$('#tabs').tabs();
+	$('#tabs1').tabs();
 	
 });
+
+function showlightbox(id, mask, msg) 
+{
+	//Get the screen height and width
+	var maskHeight = $(document).height();
+	var maskWidth = $(window).width();
+	
+	msg = msg || '';
+
+	//Set heigth and width to mask_zv to fill up the whole screen
+	$(mask).css({'width':maskWidth,'height':maskHeight});
+	
+	//transition effect		
+	$(mask).fadeIn(1000);	
+	$(mask).fadeTo("slow",0.8);	
+
+	var winH = $(document).height();
+	var winW = $(document).width();
+
+	//Set the popup window to center
+	$(id).css('top',  winH/2-$(id).height()/2);
+	$(id).css('left', winW/2-$(id).width()/2);
+	
+	if (msg != '') {
+		$('#lb_show_msg').append('<p><strong>'+msg+'</strong></p>');
+	}
+
+	//transition effect
+	$(id).fadeIn(2000); 
+	$(window).scrollTop($(id).offset().top);
+}
+
+function closelightbox(id, mask)
+{
+	$(mask).hide();
+	$(id).hide();
+};
+
