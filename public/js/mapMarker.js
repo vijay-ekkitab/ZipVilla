22647d4335e991a2beddfda99b046de5416d1945
sylvester <mailsylvester@yahoo.com>
2011-12-30 12:26:17 +0530

    Titles changed to Home Away From Home

--- public/js/mapMarker.js
+++ public/js/mapMarker.js
@@ -21,7 +21,7 @@ function mapMarker(pMapCanvas, pStartIndex, pZoom, pMapIndex)
 	if ((typeof zv_map_center_latitude === 'undefined') || 
 			(typeof zv_map_center_longitude === 'undefined')
 		  ) {
-			//$(document.getElementById(pMapCanvas)).append('<h4>Information unavailable to render Map</h4>');
+			$(document.getElementById(pMapCanvas)).append('<h4>Information unavailable to render Map</h4>');
 	  
 		zv_map_center_latitude = 22.0000;
 	  zv_map_center_longitude = 77.0000;
@@ -69,14 +69,12 @@ function mapMarker(pMapCanvas, pStartIndex, pZoom, pMapIndex)
 				+'}-{'+zv_villa_locations[i][1]+'}-{'+zv_villa_locations[i][2]+'}</h4></div>');
 	  */
 		if( zv_villa_locations[i][1] > 0 ) {   
-//alert("inside  map render");
-
 //			if (typeof pStartIndex === 'undefined')
 //				var markerImage = '/images/map_markers/red1_99/blank.png';
 //			else
 //				var markerImage = '/images/map_markers/red1_99/marker' + startIndex + '.png';
 			
-			var markerImage = '/images/map_markers/red1_99/marker' + startIndex + '.png';
+			var markerImage = '/images/map_markers/zv_1_99/marker' + startIndex + '.png';
 			
 			var image = new google.maps.MarkerImage(markerImage, new google.maps.Size(20, 34), new google.maps.Point(0, 0), new google.maps.Point(10, 34));
 			
@@ -94,11 +92,12 @@ function mapMarker(pMapCanvas, pStartIndex, pZoom, pMapIndex)
 												  function() {
 														myInfowindow[myMapIndex].setContent(this.html);
 														myInfowindow[myMapIndex].open(myMap[myMapIndex], this);
-														myInfowindow[myMapIndex].setMaxWidth(100);
+														myInfowindow[myMapIndex].setMaxWidth(50);
 													});
 					google.maps.event.trigger(myMap[myMapIndex], 'resize');
 		}
 	}
+
 	google.maps.event.trigger(myMap[myMapIndex], 'resize');
 	myMap[myMapIndex].setZoom( myMap[myMapIndex].getZoom() );
 
