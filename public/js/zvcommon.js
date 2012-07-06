c554e4fa36f93adce07ddf6a3d5f0b5e3f12a496
Ramki <ramki@ramki-Lenovo-G460.(none)>
2012-07-06 12:10:26 +0530

    added google analytics tracking code

--- public/js/zvcommon.js
+++ public/js/zvcommon.js
@@ -579,4 +579,16 @@ function mycarousel_initCallback(carousel) {
   });
 };
 
+//Google Analytics code.
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-33203611-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 // End Of File
